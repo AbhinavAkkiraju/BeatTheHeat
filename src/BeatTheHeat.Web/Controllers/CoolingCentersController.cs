58afc786388e3985e512af3cb7977a8e4ea1c457
albert-du <52804499+albert-du@users.noreply.github.com>
2022-08-05 17:27:48 -0700

    address apis

--- src/BeatTheHeat.Web/Controllers/CoolingCentersController.cs
+++ src/BeatTheHeat.Web/Controllers/CoolingCentersController.cs
@@ -7,13 +7,27 @@ using Microsoft.AspNetCore.Mvc;
 public class CoolingCentersController : ControllerBase
 {
     private readonly ICoolingCenterService _coolingCenterService;
+    private readonly IGeolocationService _geolocationService;
 
-    public CoolingCentersController(ICoolingCenterService coolingCenterService)
+    public CoolingCentersController(ICoolingCenterService coolingCenterService, IGeolocationService geolocationService)
     {
         _coolingCenterService = coolingCenterService;
+        _geolocationService = geolocationService;
     }
 
     [HttpGet("near")]
     public async Task<IEnumerable<CoolingCenterDTO>> Near(double latitude, double longitude) =>
         (await _coolingCenterService.GetNear(latitude, longitude)).Select(x => x.ToDTO());
+
+    [HttpGet("near-address")]
+    public async Task<IEnumerable<CoolingCenterDTO>> NearAddress(string address)
+    {
+        var res = await _geolocationService.GetAddressesAsync(address);
+        if (res.Count > 0)
+        {
+            var loc = res[1];
+            return (await _coolingCenterService.GetNear(loc.Lat, loc.Lon)).Select(x => x.ToDTO());
+        }
+        return Enumerable.Empty<CoolingCenterDTO>();
+    }
 }
